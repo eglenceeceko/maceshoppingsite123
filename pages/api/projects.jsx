25718c3feda9d9881edf5060b9304785e5ec6a5b
eglenceeceko <136479338+eglenceeceko@users.noreply.github.com>
2025-03-08 23:37:31 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -21,6 +21,34 @@ export default async (req, res) => {
       description: "MaceShopping olarak The Growing Darkness modunun aktif olarak developerlik hizmetini yapmaktayız. The Growing Darkness bir minecraft İtemsadder mod içeriğidir. ",
       link: "https://discord.gg/9RVKwYHw",
     },
+    {
+      id: 4,
+      image: "https://raw.githubusercontent.com/eglenceeceko/maceshoppingsite123/refs/heads/main/lib/Mace_Shopping_Logo.png",
+      name: "SpeedyNW",
+      description: "MaceShopping olarak SpeedyNW minecraft sunucusunun aktif olarak developerlik hizmetini yapmaktayız. SpeedyNW skyblock moduna sahip bir minecraft sunucusudur.",
+      link: "https://discord.gg/49VsrUBM",
+    },
+    {
+      id: 5,
+      image: "https://raw.githubusercontent.com/eglenceeceko/maceshoppingsite123/refs/heads/main/lib/Mace_Shopping_Logo.png",
+      name: "Ottomania",
+      description: "MaceShopping olarak Ottomania minecraft sunucusunun aktif olarak developerlik hizmetini yapmaktayız. Ottomania towny moduna sahip bir minecraft sunucusudur.",
+      link: "https://discord.gg/Xva7P5Dp",
+    },
+    {
+      id: 6,
+      image: "https://raw.githubusercontent.com/eglenceeceko/maceshoppingsite123/refs/heads/main/lib/Mace_Shopping_Logo.png",
+      name: "Rain Network",
+      description: "MaceShopping olarak Rain Network minecraft sunucusunun aktif olarak developerlik hizmetini yapmaktayız. Rain Network skyblock moduna sahip bir minecraft sunucusudur.",
+      link: "https://discord.gg/8vRfQCQU",
+    },
+    {
+      id: 7,
+      image: "https://raw.githubusercontent.com/eglenceeceko/maceshoppingsite123/refs/heads/main/lib/Mace_Shopping_Logo.png",
+      name: "The Cyrus TV",
+      description: "MaceShopping olarak The Cyrus TV minecraft sunucusunun aktif olarak developerlik hizmetini yapmaktayız. The Cyrus TV towny moduna sahip bir minecraft sunucusudur.",
+      link: "https://discord.gg/rjZnYtRMgR",
+    },
   ];
   res.status(200).json(data);
 };
