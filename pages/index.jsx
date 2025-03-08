c69551120a251f78d8cf97f829ca76684a68ee5f
eglenceeceko <136479338+eglenceeceko@users.noreply.github.com>
2025-03-09 01:04:34 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,40 +21,43 @@ export default function Home() {
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
-        <p>Hi! I'm <strong>Umut</strong>, a 19-year-old software developer and content creator based in London, UK. Originally from Antalya, Turkey, I specialize in technology, software, and cryptocurrency.</p>
+        <p>Hepinize selamlar! <strong>MaceShopping</strong>, mağazası olarak 2023 yılında kurulduk fakat 2025 tarihinde işleve geçtik. MaceShopping olarak discord botu, plugin, itemsadder rank tasarım, developer, sunucunuza özel sıfırdan paket, map hizmetleri vermekteyiz.</p>
         <br></br>
-        <h1><strong>EXPERIENCE</strong></h1>
+        <h1><strong>DENEYİM</strong></h1>
         <ul>
-            <li><strong>* Community Management:</strong> With 5 years of experience, I’ve worked on various projects, building and managing communities and developing social media strategies. During this time, I’ve successfully coordinated communities with thousands of members, adding value to multiple projects.</li>
-            <li><strong>* Software Development:</strong> As a full-stack web developer, I’ve been involved in various projects, including <strong>Code Share</strong>, a public platform for sharing software.</li>
-            <li><strong>* Content Creation:</strong> I create expert-level content about software and cryptocurrency on social media, where I’ve gained a following of over <strong>55,000 people</strong>.</li>
+            <li><strong>* Discord Bot:</strong> 2019 tarihinden beri çeşitli discord botları kodlayıp kullanmış bulunmaktayım. Hatta 1 adet botum onaylanma mesajı aldık, botumuz kısa bir süreli onaylı kalıp kendi isteğimle kapattım.</li>
+            <li><strong>* Plugin Hizmeti:</strong> 2016 tarihinden beri minecraft oynamaktayım. Sunucu işine ilk olarak 2023 tarihinde atıldım, ne yazıkki sunucum tutmadı ve kapatmak zorunda kaldım.</li>
+            <li><strong>* İtemsadder Rank Tasarım:</strong> 2020 tarihinden beri grafik hizmeti sunuyorum, bir aralar Mace Design diye bir firmam daha vardı ama özel sebeplerden dolayı kapatmak zorunda kaldım.</li>
+            <li><strong>* Developer Hizmeti:</strong> 2020 tarihinden beri yazılım işi ile uğraşıyorum. Hatta okul projem için bir oyun kodlamışlığım bile oldu. Yazılım olarak discord botları, skript kodlaması bile yaptım.</li>
+            <li><strong>* Sıfırdan Paket:</strong> 2023 tarihinden beri sıfırdan paket tasarlıyorum. İlk olarak kendi minecraft sunucumu kodlamıştım.</li>
+            <li><strong>* Map Hizmeti:</strong> 2024 tarihinden beri minecraft sunucuları için sıfırdan harita tasarlıyorum.</li>
         </ul>
         <br></br>
-        <h1><strong>SOCIAL MEDIA AND COMMUNITY LINKS</strong></h1>
+        <h1><strong>SOSYAL MEDYA HESAPLARIMIZ</strong></h1>
         <ul>
-            <li><a href="https://facebook.com/umutxyp" target="_blank">* umutxyp - Facebook</a></li>
-            <li><a href="https://instagram.com/umutxyp" target="_blank">* umutxyp - Instagram</a></li>
-            <li><a href="https://t.me/umutland" target="_blank">* umutxyp - Telegram</a></li>
-            <li><a href="https://youtube.com/umutxyp" target="_blank">* umutxyp - YouTube</a></li>
+            <li><a href="https://facebook.com/umutxyp" target="_blank">* BAKIMDA - Facebook</a></li>
+            <li><a href="https://instagram.com/maceshoppingg" target="_blank">* maceshoppingg - Instagram</a></li>
+            <li><a href="https://discord.gg/dhXcFgaxr2" target="_blank">* BAKIMDA - Telegram</a></li>
+            <li><a href="https://youtube.com/maceshopping" target="_blank">* maceshopping - YouTube</a></li>
         </ul>
 <br></br>
-        <h1><strong>PROJECTS</strong></h1>
-        <p><strong>Code Share:</strong> My first project was a public platform for developers to share their software. This initiative helped me gain significant experience in planning, development, and project management. (<a href="https://codeshare.me" rel="follow" target="_blank">codeshare.me</a>)</p>
+        <h1><strong>PROJELERİMİZ</strong></h1>
+        <p><strong>MaceMC:</strong> MaceShopping mağazasının resmi minecraft sunucusu. (<a href="https://discord.gg/dhXcFgaxr2" rel="follow" target="_blank">BAKIMDA</a>)</p>
 <br></br>
-        <h1><strong>GOALS</strong></h1>
-        <p>I aim to reach wider audiences by sharing knowledge in the fields of cryptocurrency and technology. With a focus on innovative thinking and professionalism, I strive to make a difference, especially in community management and social media.</p>
+        <h1><strong>HEDEFLERİMİZ</strong></h1>
+        <p>MaceShopping olarak ilk hedefimiz daha büyük kitlelere ulaşmak ve müşteri dostu bir firma olarak kalmaya devam etmemiz. Hiçbir zaman para için bu işe girmedim, girmeyeceğimde. Kalite olarak aynı kalmamız hatta fiyatlarımızın dahada ucuzlayarak müşterilerimizin yanında olmak istememizdir.</p>
         <br></br>
-        <p>If you're interested in learning more about software, crypto, or community management, let’s connect on my social media channels! 😊</p>
+        <p>İkinci hedefim ise daha büyük kitleye ulaşmak ve büyük minecraft sunucularının geliştirici yani developerlik hizmetini üstlenmek. 😊</p>
                 
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutxyp" src={`https://ugc.production.linktr.ee/6RJk9s2pQZ2yAdcxw3Ir_TXX2dLlNmwM2OFdf?io=true&size=avatar-v3_0`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <img alt="maceshopping" src={`https://github.com/eglenceeceko/maceshoppingsite123/blob/main/lib/Mace_Shopping_Logo.png?raw=true`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
               </div>
               
             </div>
             <br></br><br></br>
-            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Content Creator / Community Manager</span>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">BU KISIM BAKIMDA / MaceShopping</span>
           </div>
       </div>
     </>
