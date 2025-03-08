cef13f93634132cb2ea53f62e9c7e89bb4703810
eglenceeceko <136479338+eglenceeceko@users.noreply.github.com>
2025-03-09 01:35:21 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -11,19 +11,19 @@ export default function Home() {
     <>
       <div className="py-20">
         <p className="text-3xl text-white font-semibold text-center">İletişim Desteği</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">Aşağıdaki adreslerden benimle iletişim kurabilirsin.</p>
+        <p className="text-xl text-white/50 font-normal text-center mb-5">Aşağıdaki adreslerden bizimle iletişim kurabilirsin.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
             <a target="_blank" rel="norefferer" href="https://discord.gg/dhXcFgaxr2">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">Mace Shopping</p>
+                        <p className="font-semibold text-xl">Discord Sunucumuza Gitmek İçin Tıkla!</p>
                     </div>
                 </a>
                 <a target="_blank" rel="norefferer" href="https://instagram.com/maceshoppingg">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">Mace Shopping</p>
+                        <p className="font-semibold text-xl">İnstagram Hesabımıza Gitmek İçin Tıkla!</p>
                     </div>
                 </a>
                 <a target="_blank" rel="norefferer" href="https://www.itemsatis.com/p/maceshopping">
@@ -35,7 +35,7 @@ export default function Home() {
                 <a href="mailto:bakımda">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">BAKIMDA</p>
+                        <p className="font-semibold text-xl">Mail Adresimiz Bakımdadır.</p>
                     </div>
                 </a>
             </div>
