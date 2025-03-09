176a8e31402ae42f7c6ec88baac560d78f46ba66
eglenceeceko <136479338+eglenceeceko@users.noreply.github.com>
2025-03-09 15:49:25 +0300

    Update blogs.jsx

--- pages/blogs.jsx
+++ pages/blogs.jsx
@@ -4,8 +4,8 @@ import swr from '../lib/swr';
 
 
 export default function Home() {
-  const { data: _projects } = swr('/api/blogs');
-  const projects = _projects ? _projects : null;
+  const { data: _blogs } = swr('/api/blogs');
+  const blogs = _blogs ? _blogs : null;
 
   return (
     <>
@@ -14,9 +14,9 @@ export default function Home() {
         <p className="text-xl text-white/50 font-normal text-center mb-5">Hizmet sağladığımız sunucular listesi.</p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 w-full gap-4 items-center mt-2">
-          {_projects ? (
-            projects ? (
-              projects.map((_, __) => (
+          {_blogs ? (
+            blogs ? (
+              blogs.map((_, __) => (
                 <a href={_.link} target="_blank" rel="noreferrer" key={__} className="bg-neutral-800/10 p-4 hover:bg-neutral-800/20 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full">
                   <img alt="umut" src={_.image} width="1024" className="rounded-lg" height="512" />
                   <p className="text-xl font-semibold mt-5">{_.name}</p>
